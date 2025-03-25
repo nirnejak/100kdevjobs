eb6c7fa242cf5a1b48311a190c2081b5cd62a4fe
Jitendra Nirnejak <jeetnirnejak@gmail.com>
2025-03-26 04:43:38 +0530

    Initial commit

---
+++ config.ts
@@ -0,0 +1,23 @@
+const config = {
+  baseUrl: "https://example.com",
+  appName: "Next.js TypeScript Starter",
+  appDescription:
+    "Next.js started configured TypeScript, ESLint, Tailwind, with some additional configuration",
+  creator: "<Creator Name>",
+  authorName: "<Author Name>",
+  authorUrl: "<Author Name or Email>",
+  keywords: [
+    "Next.js",
+    "TailwindCSS",
+    "Framer Motion",
+    "TypeScript",
+    "Akar Icons",
+    "ESLint",
+    "Prettier",
+    "JavaScript",
+    "Postgres",
+  ],
+  twitterSite: "@site",
+  twitterCreator: "@creator",
+}
+export default config
