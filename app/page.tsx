eeec9b5dcbfb9aa1ddd220f689bc3ea6561330e7
Jitendra Nirnejak <jeetnirnejak@gmail.com>
2025-03-26 06:27:19 +0530

    Hero content moved to HomeHero

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,9 @@
 import * as React from "react"
 
-import * as motion from "motion/react-client"
-
-import { BASE_TRANSITION } from "@/utils/animation"
 import { getMetadata } from "@/utils/metadata"
 
 import JobPosts from "@/components/JobPosts"
+import HomeHero from "@/components/HomeHero"
 
 export const metadata = getMetadata({
   path: "/",
@@ -15,18 +13,8 @@ export const metadata = getMetadata({
 
 const Home: React.FC = () => {
   return (
-    <main className="">
-      <section className="mt-52 mb-24 max-w-5xl mx-auto">
-        <motion.h1
-          initial={{ translateY: 20, opacity: 0, filter: `blur(10px)` }}
-          animate={{ translateY: 0, opacity: 1, filter: "none" }}
-          transition={{ delay: 0, ...BASE_TRANSITION }}
-          className="text-6xl font-bold tracking-tighter leading-normal text-zinc-300"
-        >
-          Find tech jobs that pays <br />
-          $100k+ in USA, Europe, India.
-        </motion.h1>
-      </section>
+    <main>
+      <HomeHero />
       <JobPosts />
     </main>
   )
---
+++ components/HomeHero.tsx
@@ -0,0 +1,17 @@
+"use client"
+import * as React from "react"
+
+interface Props {}
+
+const HomeHero: React.FC<Props> = () => {
+  return (
+    <section className="mt-52 mb-24 max-w-5xl mx-auto">
+      <h1 className="text-6xl font-bold tracking-tighter leading-normal text-zinc-300">
+        Find tech jobs that pays <br />
+        $100k+ in USA, Europe, India.
+      </h1>
+    </section>
+  )
+}
+
+export default HomeHero
