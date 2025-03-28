58885d5c4bfef9fce1df23091325f2c1649b0108
Jitendra Nirnejak <jeetnirnejak@gmail.com>
2025-03-28 16:18:47 +0530

    Home Hero: content and subscription form added

--- components/HomeHero.tsx
+++ components/HomeHero.tsx
@@ -5,10 +5,27 @@ interface Props {}
 const HomeHero: React.FC<Props> = () => {
   return (
     <section className="mt-60 mb-48 max-w-5xl mx-auto">
-      <h1 className="text-6xl font-bold tracking-tighter leading-normal text-zinc-300">
+      <h1 className="text-6xl font-bold tracking-tighter leading-normal text-zinc-300 mb-6">
         Find tech jobs that pays <br />
         <span className="text-green-500">$100k+</span> in USA, Europe, India.
       </h1>
+      <p className="text-zinc-400 text-lg mb-8">
+        Get notified about high paying jobs at top tech companies.
+      </p>
+      <form className="flex max-w-[400px] mb-2">
+        <input
+          type="email"
+          placeholder="Enter your email"
+          className="flex-1 bg-zinc-800 px-4 py-2.5 text-zinc-200 text-sm"
+        />
+        <button
+          type="submit"
+          className="bg-green-800 text-white px-4 py-2.5 text-sm"
+        >
+          Get notified
+        </button>
+      </form>
+      <p className="text-zinc-400 text-sm">No spam. Unsubscribe anytime.</p>
     </section>
   )
 }
