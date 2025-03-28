ed28bd1441e3ef2ef714b3a6961281eaf3c358cb
Jitendra Nirnejak <jeetnirnejak@gmail.com>
2025-03-28 16:15:12 +0530

    Header: width issue fixed

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,22 +5,30 @@ import { Link } from "next-view-transitions"
 
 const Header: React.FC = () => {
   return (
-    <header className="max-w-5xl mx-auto flex justify-between items-center py-4 text-zinc-300 fixed top-0 left-0 right-0 z-10 bg-zinc-900">
-      <Link href="/" className="text-2xl font-bold">
-        100KDevJobs
-      </Link>
-      <nav className="flex items-center gap-6">
-        <Link href="/">FAQ</Link>
-        <Link href="/jobs">Blog</Link>
-        <Link href="/about">Ideas + Feedback</Link>
+    <div className="fixed top-0 left-0 right-0 z-10 bg-zinc-900">
+      <header className="max-w-5xl mx-auto flex justify-between items-center py-4 text-zinc-300">
         <Link
-          href={"/submit"}
-          className="text-sm text-zinc-800 bg-zinc-200 hover:bg-zinc-400 transition-colors focus:bg-zinc-400 outline-none py-2 px-7 cursor-pointer"
+          href="/"
+          className="text-2xl leading-none font-bold tracking-tight flex gap-2"
         >
-          Post a Job
+          <span>100KDevJobs</span>
+          <span className="font-mono text-zinc-200 bg-green-800 rounded-full px-3 py-1 text-xs ">
+            $ 100K+
+          </span>
         </Link>
-      </nav>
-    </header>
+        <nav className="flex items-center gap-6">
+          <Link href="/">FAQ</Link>
+          <Link href="/jobs">Blog</Link>
+          <Link href="/about">Ideas + Feedback</Link>
+          <Link
+            href={"/submit"}
+            className="text-sm text-zinc-800 bg-zinc-200 hover:bg-zinc-400 transition-colors focus:bg-zinc-400 outline-none py-2 px-7 cursor-pointer"
+          >
+            Post a Job
+          </Link>
+        </nav>
+      </header>
+    </div>
   )
 }
 
