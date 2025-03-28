096b263eb0bae11c983804f8a7200cb5d875b713
Jitendra Nirnejak <jeetnirnejak@gmail.com>
2025-03-29 04:29:13 +0530

    sitemap url format updated

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -7,19 +7,19 @@ const { baseUrl } = config
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   const staticRoutes = [
     {
-      url: `${baseUrl}/`,
+      url: baseUrl + `/`,
       lastModified: new Date(),
       changeFrequency: "daily" as const,
       priority: 1,
     },
     {
-      url: `${baseUrl}/faq/`,
+      url: baseUrl + `/faq/`,
       lastModified: new Date(),
       changeFrequency: "weekly" as const,
       priority: 0.8,
     },
     {
-      url: `${baseUrl}/ideas-feedback/`,
+      url: baseUrl + `/ideas-feedback/`,
       lastModified: new Date(),
       changeFrequency: "weekly" as const,
       priority: 0.8,
