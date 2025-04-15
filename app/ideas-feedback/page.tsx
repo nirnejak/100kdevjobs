daea4b89b26af38feb17203b753bed19505c6df7
Jitendra Nirnejak <jeetnirnejak@gmail.com>
2025-04-15 18:34:46 +0530

    Ideas & Feedback page added

---
+++ app/ideas-feedback/page.tsx
@@ -0,0 +1,19 @@
+import * as React from "react"
+
+import { getMetadata } from "@/utils/metadata"
+
+export const metadata = getMetadata({
+  path: "/ideas-feedback",
+  title: "Ideas & Feedback | 100k Dev Jobs",
+  description: "Share your ideas and feedback for 100k Dev Jobs",
+})
+
+const IdeasFeedbackPage: React.FC = () => {
+  return (
+    <main>
+      <h1>Ideas & Feedback</h1>
+    </main>
+  )
+}
+
+export default IdeasFeedbackPage
