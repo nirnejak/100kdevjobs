4ac96e4aa4a0a03a8b89c24a9363b4cd813480a0
Jitendra Nirnejak <jeetnirnejak@gmail.com>
2025-04-15 18:34:27 +0530

    FAQ page added

---
+++ app/faq/page.tsx
@@ -0,0 +1,19 @@
+import * as React from "react"
+
+import { getMetadata } from "@/utils/metadata"
+
+export const metadata = getMetadata({
+  path: "/faq",
+  title: "FAQ | 100k Dev Jobs",
+  description: "Frequently asked questions about 100k Dev Jobs",
+})
+
+const FAQPage: React.FC = () => {
+  return (
+    <main>
+      <h1>FAQ</h1>
+    </main>
+  )
+}
+
+export default FAQPage
