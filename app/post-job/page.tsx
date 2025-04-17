74b6d3f17e1838b9f657b5931e3ef7e57843acb4
Jitendra Nirnejak <jeetnirnejak@gmail.com>
2025-04-18 04:33:36 +0530

    Post Job added

--- app/post-job/page.tsx
+++ app/post-job/page.tsx
@@ -1,9 +1,10 @@
 import * as React from "react"
-
 import * as motion from "motion/react-client"
 
 import { getMetadata } from "@/utils/metadata"
 import { BASE_TRANSITION } from "@/utils/animation"
+
+import PostJobForm from "@/components/PostJobForm"
 
 export const metadata = getMetadata({
   path: "/post-job/",
@@ -14,15 +15,17 @@ export const metadata = getMetadata({
 const PostJobPage: React.FC = () => {
   return (
     <main className="pt-48">
-      <section className="pb-44 relative">
+      <section className="pb-44 relative max-w-2xl mx-auto px-4">
         <motion.h1
           initial={{ translateY: 10, opacity: 0, filter: "blur(5px)" }}
           animate={{ translateY: 0, opacity: 1, filter: "none" }}
           transition={{ ...BASE_TRANSITION, duration: 0.2, delay: 0 }}
-          className="font-display text-4xl font-bold tracking-tighter text-center leading-tight text-zinc-300"
+          className="font-display text-4xl font-bold tracking-tighter text-center leading-tight text-zinc-300 mb-12"
         >
           Post a Job
         </motion.h1>
+
+        <PostJobForm />
       </section>
     </main>
   )
